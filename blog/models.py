47e67c7ef76468ab47eb772fa4d8ecf13af37ee4
Robin Phetsavong <robinphetsa@icloud.com>
2016-07-08 20:46:33 +0000

    model changes

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=200)
     
     def __str__(self):
